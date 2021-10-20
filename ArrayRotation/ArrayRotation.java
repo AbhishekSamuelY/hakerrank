10bcd34259a19bb142ad8d1316c016cdefea6d2e
abhishek <62393400+AbhishekSamuelY@users.noreply.github.com>
2021-10-20 12:49:35 +0530

    array rotation

---
+++ ArrayRotation/ArrayRotation.java
@@ -0,0 +1,41 @@
+/*
+ 
+       d8888 888888b.   888    888 8888888 .d8888b.  888    888 8888888888 888    d8P   .d8888b.        d8888 888b     d888 888     888 8888888888 888    Y88b   d88P 
+      d88888 888  "88b  888    888   888  d88P  Y88b 888    888 888        888   d8P   d88P  Y88b      d88888 8888b   d8888 888     888 888        888     Y88b d88P  
+     d88P888 888  .88P  888    888   888  Y88b.      888    888 888        888  d8P    Y88b.          d88P888 88888b.d88888 888     888 888        888      Y88o88P   
+    d88P 888 8888888K.  8888888888   888   "Y888b.   8888888888 8888888    888d88K      "Y888b.      d88P 888 888Y88888P888 888     888 8888888    888       Y888P    
+   d88P  888 888  "Y88b 888    888   888      "Y88b. 888    888 888        8888888b        "Y88b.   d88P  888 888 Y888P 888 888     888 888        888        888     
+  d88P   888 888    888 888    888   888        "888 888    888 888        888  Y88b         "888  d88P   888 888  Y8P  888 888     888 888        888        888     
+ d8888888888 888   d88P 888    888   888  Y88b  d88P 888    888 888        888   Y88b  Y88b  d88P d8888888888 888   "   888 Y88b. .d88P 888        888        888     
+d88P     888 8888888P"  888    888 8888888 "Y8888P"  888    888 8888888888 888    Y88b  "Y8888P" d88P     888 888       888  "Y88888P"  8888888888 88888888   888     
+======================================================================================================================================================================
+email: abhisheksamuel.y@gmail.com
+github: abhisheksamuely@github
+                                                                                                                                                                                                                                                                              
+*/
+package com.abhisheksamuely.hakerrank;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayRotation {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+		int size = list.size();
+		int[] array = new int[size];
+		int rotation = 2;
+		
+		
+		for(int i = 0; i < size;i++){
+	           array[(i+size-rotation)%size] = list.get(i);
+	       }
+		
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+
+}
