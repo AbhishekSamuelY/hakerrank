3b1e3bf29ae325bc1d3565abee1e4350ea9264ff
abhishek <62393400+AbhishekSamuelY@users.noreply.github.com>
2021-10-20 16:12:25 +0530

    sparse arrays

---
+++ SparseArrays/SparseArrays.java
@@ -0,0 +1,62 @@
+/*
+ 
+       d8888 888888b.   888    888 8888888 .d8888b.  888    888 8888888888 888    d8P   .d8888b.        d8888 888b     d888 888     888 8888888888 888    Y88b   d88P 
+      d88888 888  "88b  888    888   888  d88P  Y88b 888    888 888        888   d8P   d88P  Y88b      d88888 8888b   d8888 888     888 888        888     Y88b d88P  
+     d88P888 888  .88P  888    888   888  Y88b.      888    888 888        888  d8P    Y88b.          d88P888 88888b.d88888 888     888 888        888      Y88o88P   
+    d88P 888 8888888K.  8888888888   888   "Y888b.   8888888888 8888888    888d88K      "Y888b.      d88P 888 888Y88888P888 888     888 8888888    888       Y888P    
+   d88P  888 888  "Y88b 888    888   888      "Y88b. 888    888 888        8888888b        "Y88b.   d88P  888 888 Y888P 888 888     888 888        888        888     
+  d88P   888 888    888 888    888   888        "888 888    888 888        888  Y88b         "888  d88P   888 888  Y8P  888 888     888 888        888        888     
+ d8888888888 888   d88P 888    888   888  Y88b  d88P 888    888 888        888   Y88b  Y88b  d88P d8888888888 888   "   888 Y88b. .d88P 888        888        888     
+d88P     888 8888888P"  888    888 8888888 "Y8888P"  888    888 8888888888 888    Y88b  "Y8888P" d88P     888 888       888  "Y88888P"  8888888888 88888888   888     
+======================================================================================================================================================================
+email: abhisheksamuel.y@gmail.com
+github: abhisheksamuely@github
+                                                                                                                                                                                                                                                                              
+*/
+package com.abhisheksamuely.hakerrank;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SparseArrays {
+
+	public static void main(String[] args) {
+
+		List<String> strings = new ArrayList<>(Arrays.asList("ab", "ab", "abc"));
+		List<String> queries = new ArrayList<>(Arrays.asList("ab", "abc", "bc"));
+		Map<String, Integer> map = new HashMap<>();
+		int[] array = new int[queries.size()];
+
+		/* solution 1:
+		 * for (int i = 0; i < queries.size(); i++) { array[i] =
+		 * Collections.frequency(strings, queries.get(i)); }
+		 */
+
+		for (String str : strings) {
+			if (map.containsKey(str)) {
+				map.put(str, map.get(str) + 1);
+			} else {
+				map.put(str, 1);
+			}
+		}
+
+		int i = 0;
+		for (String str : queries) {
+			if (map.containsKey(str)) {
+				array[i] = map.get(str);
+			} else {
+				array[i] = 0;
+			}
+			i++;
+		}
+
+		for (int j = 0; j < array.length; j++) {
+			System.out.print(array[j] + "\t");
+		}
+	}
+
+}
