6df76e513902325f66f1a561bf074d7a8262bb30
abhishek <62393400+AbhishekSamuelY@users.noreply.github.com>
2021-11-11 04:13:41 +0530

    ArrayManipulation

---
+++ ArrayManipulation/ArrayManipulation .java
@@ -0,0 +1,52 @@
+/*
+ 
+       d8888 888888b.   888    888 8888888 .d8888b.  888    888 8888888888 888    d8P   .d8888b.        d8888 888b     d888 888     888 8888888888 888    Y88b   d88P 
+      d88888 888  "88b  888    888   888  d88P  Y88b 888    888 888        888   d8P   d88P  Y88b      d88888 8888b   d8888 888     888 888        888     Y88b d88P  
+     d88P888 888  .88P  888    888   888  Y88b.      888    888 888        888  d8P    Y88b.          d88P888 88888b.d88888 888     888 888        888      Y88o88P   
+    d88P 888 8888888K.  8888888888   888   "Y888b.   8888888888 8888888    888d88K      "Y888b.      d88P 888 888Y88888P888 888     888 8888888    888       Y888P    
+   d88P  888 888  "Y88b 888    888   888      "Y88b. 888    888 888        8888888b        "Y88b.   d88P  888 888 Y888P 888 888     888 888        888        888     
+  d88P   888 888    888 888    888   888        "888 888    888 888        888  Y88b         "888  d88P   888 888  Y8P  888 888     888 888        888        888     
+ d8888888888 888   d88P 888    888   888  Y88b  d88P 888    888 888        888   Y88b  Y88b  d88P d8888888888 888   "   888 Y88b. .d88P 888        888        888     
+d88P     888 8888888P"  888    888 8888888 "Y8888P"  888    888 8888888888 888    Y88b  "Y8888P" d88P     888 888       888  "Y88888P"  8888888888 88888888   888     
+======================================================================================================================================================================
+email: abhisheksamuel.y@gmail.com
+github: abhisheksamuely@github
+                                                                                                                                                                                                                                                                              
+*/
+package com.abhisheksamuely.hakerrank;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayManipulation {
+
+	static long[] array = null;
+
+	public static void main(String[] args) {
+
+		List<List<Integer>> queries = new ArrayList<>();
+		queries.add(Arrays.asList(1, 2, 100));
+		queries.add(Arrays.asList(2, 5, 100));
+		queries.add(Arrays.asList(3, 4, 100));
+
+		int n = 5;
+
+		int[] array = new int[n + 2];
+		long max = Long.MIN_VALUE;
+		long sum = 0;
+
+		for (List<Integer> query : queries) {
+			array[query.get(0)] += query.get(2);
+			array[query.get(1) + 1] -= query.get(2);
+		}
+
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+			max = Math.max(max, sum);
+		}
+
+		System.out.print(max);
+	}
+
+}
